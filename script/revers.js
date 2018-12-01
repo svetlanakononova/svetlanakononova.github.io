ba0337688c5cd78205b8e32c40409d16f6439bda
Svetlana Kononova <svetlana.kononova@gmail.com>
2018-12-01 12:51:37 +0300

    Repair incorrect function

--- script/revers.js
+++ script/revers.js
@@ -1,9 +1,7 @@
   const reverse = (str) => {
       if (str === '') return str;
-      const reversefunct = (res,subs) => 
-      reversefunct (res + subs [ subs.length - 1 ], subs.substr (0, subs.length - 1));
-
+      const reversefunct = (res,subs) => (subs==='') ?res:reversefunct (res + subs [ subs.length - 1 ], subs.substr (0, subs.length - 1));
       return reversefunct ('',str);
   };
 
-
+  console.log (reverse ('1234567890'));
